a687258ff6ad61768662c890a86725a6e80ee3f3
jimmyfigueroa <jimmyfigueroa@Jimmys-MacBook-Pro.local>
2015-05-09 12:35:51 -0600

    introducir dimension moneda en vistal_real

--- vistal_real.sql
+++ vistal_real.sql
@@ -1,54 +1,111 @@
-DROP VIEW vistal_real 
+drop view vistal_real
 
-CREATE VIEW vistal_real 
-AS 
-  (SELECT centrocostocodigo, 
-          cuentacontablecodigo, 
-          mes_saldo, 
-          ano_saldo, 
-          centro_costo, 
-          Replace(cuenta_contable, ',', '') AS cuenta_contable, 
-          tipo_cuenta, 
-          subtipo_cuenta, 
-          saldo_normal, 
-          fecha_saldo, 
-          trimestre_saldo, 
-          saldoinifiscallocal, 
-          debitofisclocal, 
-          creditofisclocal, 
-          saldofisclocal, 
-          saldoinifiscaldolar, 
-          creditofiscdolar, 
-          saldofiscdolar 
-   FROM   exactus.procomer.softlandbi_cg_saldcuencontable 
-   WHERE  cuentacontablecodigo <> '205-007-000-000' 
-   UNION ALL 
-   SELECT centro_costo_codigo        AS centrocostocodigo, 
-          cuenta_contable_codigo     AS cuentacontablecodigo, 
-          mes_saldo, 
-          ano_saldo, 
-          Max(ccosto.descripcion)    AS centro_costo, 
-          Max(ccontable.descripcion) AS cuenta_contable, 
-          Max('')                    AS tipo_cuenta, 
-          Max('')                    AS subtipo_cuenta, 
-          Max('')                    AS saldo_normal, 
-          Max('')                    AS fecha_saldo, 
-          Max('')                    AS trimestre_saldo, 
-          Sum(0)                     AS saldoinifiscallocal, 
-          Sum(debito_fisc_local)     AS debitofisclocal, 
-          Sum(0)                     AS creditofisclocal, 
-          Sum(0)                     AS saldofisclocal, 
-          Sum(0)                     AS saldoinifiscaldolar, 
-          Sum(0)                     AS creditofiscdolar, 
-          Sum(0)                     AS saldofiscdolar 
-   FROM   exactus.dbo.activosfijos_temp 
-          JOIN exactus.procomer.centro_costo ccosto 
-            ON exactus.dbo.activosfijos_temp.centro_costo_codigo = 
-               ccosto.centro_costo 
-          JOIN exactus.procomer.cuenta_contable ccontable 
-            ON exactus.dbo.activosfijos_temp.cuenta_contable_codigo = 
-               ccontable.cuenta_contable 
-   GROUP  BY centro_costo_codigo, 
-             cuenta_contable_codigo, 
-             mes_saldo, 
-             ano_saldo) 
+create view vistal_real as (
+(
+SELECT centrocostocodigo, 
+       cuentacontablecodigo, 
+       mes_saldo, 
+       ano_saldo, 
+       centro_costo, 
+       Replace(cuenta_contable, ',', '') AS cuenta_contable, 
+       'CRC' as moneda,
+       tipo_cuenta, 
+       subtipo_cuenta, 
+       saldo_normal, 
+       fecha_saldo, 
+       trimestre_saldo, 
+       saldoinifiscallocal, 
+       debitofisclocal, 
+       creditofisclocal, 
+       saldofisclocal, 
+       saldoinifiscaldolar, 
+       creditofiscdolar, 
+       saldofiscdolar
+FROM  exactus.dbo.jimmy_saldCuenContable
+WHERE cuentacontablecodigo <> '205-007-000-000'
+UNION ALL
+SELECT centrocostocodigo, 
+       cuentacontablecodigo, 
+       mes_saldo, 
+       ano_saldo, 
+       centro_costo, 
+       Replace(cuenta_contable, ',', '') AS cuenta_contable, 
+       'USD' as moneda,
+       tipo_cuenta, 
+       subtipo_cuenta, 
+       saldo_normal, 
+       fecha_saldo, 
+       trimestre_saldo, 
+       saldoinifiscallocal, 
+       debitofiscdolar as debitofisclocal, 
+       creditofiscdolar as creditofisclocal, 
+       saldofisclocal, 
+       saldoinifiscaldolar, 
+       creditofiscdolar, 
+       saldofiscdolar
+FROM  exactus.dbo.jimmy_saldCuenContable
+WHERE cuentacontablecodigo <> '205-007-000-000'
+)
+UNION ALL
+SELECT centro_costo_codigo as centrocostocodigo, 
+       cuenta_contable_codigo as cuentacontablecodigo, 
+  mes_saldo, 
+       ano_saldo, 
+       max(Replace(ccosto.descripcion, ',', '')) as centro_costo, 
+       max(Replace(ccontable.DESCRIPCION, ',', '')) AS cuenta_contable, 
+       max('CRC') as moneda,
+       max('') as tipo_cuenta, 
+       max('') as subtipo_cuenta, 
+       max('') as saldo_normal, 
+       max('') as fecha_saldo, 
+       max('') as trimestre_saldo, 
+       sum(0) as saldoinifiscallocal, 
+       sum(debito_fisc_local) as debitofisclocal, 
+       sum(0) as creditofisclocal, 
+       sum(0) as saldofisclocal, 
+       sum(0) as saldoinifiscaldolar, 
+       sum(0) as creditofiscdolar, 
+       sum(0) as saldofiscdolar
+FROM  exactus.dbo.activosfijos_temp 
+JOIN  exactus.procomer.CENTRO_COSTO ccosto
+  ON  exactus.dbo.activosfijos_temp.centro_costo_codigo = ccosto.CENTRO_COSTO
+JOIN  exactus.procomer.CUENTA_CONTABLE ccontable
+  ON  exactus.dbo.activosfijos_temp.cuenta_contable_codigo = ccontable.CUENTA_CONTABLE
+group by 
+       centro_costo_codigo,
+       cuenta_contable_codigo,
+  mes_saldo, 
+       ano_saldo
+
+UNION ALL
+
+SELECT centro_costo_codigo as centrocostocodigo, 
+       cuenta_contable_codigo as cuentacontablecodigo, 
+       mes_saldo, 
+       ano_saldo, 
+       max(Replace(ccosto.descripcion, ',', '')) as centro_costo, 
+       max(Replace(ccontable.DESCRIPCION, ',', '')) AS cuenta_contable, 
+       max('USD') as moneda,
+       max('') as tipo_cuenta, 
+       max('') as subtipo_cuenta, 
+       max('') as saldo_normal, 
+       max('') as fecha_saldo, 
+       max('') as trimestre_saldo, 
+       sum(0) as saldoinifiscallocal, 
+       sum(debito_fisc_dolar) as debitofisclocal, 
+       sum(0) as creditofisclocal, 
+       sum(0) as saldofisclocal, 
+       sum(0) as saldoinifiscaldolar, 
+       sum(0) as creditofiscdolar, 
+       sum(0) as saldofiscdolar
+FROM  exactus.dbo.activosfijos_temp 
+JOIN  exactus.procomer.CENTRO_COSTO ccosto
+  ON  exactus.dbo.activosfijos_temp.centro_costo_codigo = ccosto.CENTRO_COSTO
+JOIN  exactus.procomer.CUENTA_CONTABLE ccontable
+  ON  exactus.dbo.activosfijos_temp.cuenta_contable_codigo = ccontable.CUENTA_CONTABLE
+group by 
+       centro_costo_codigo,
+       cuenta_contable_codigo,
+  mes_saldo,
+       ano_saldo
+)
