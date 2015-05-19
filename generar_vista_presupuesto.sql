a4b4a2403a197238a29beda305245a77555200c9
jimmyfigueroa <jimmyfigueroa@Jimmys-MacBook-Pro.local>
2015-05-19 08:49:06 -0600

    versión 1.0 liberara

--- generar_vista_presupuesto.sql
+++ generar_vista_presupuesto.sql
@@ -1,33 +1,28 @@
 USE [EXACTUS]
 GO
-
-/****** Object:  StoredProcedure [dbo].[generar_vista_presupuesto]    Script Date: 5/9/2015 5:49:30 PM ******/
+/****** Object:  StoredProcedure [dbo].[generar_vista_presupuesto]    Script Date: 05/18/2015 13:39:58 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE procedure [dbo].[generar_vista_presupuesto]
+ 
+ALTER procedure [dbo].[generar_vista_presupuesto]
 (
     @year       varchar(100),
     @month      varchar(100)
 )
 as
-
+ 
 declare @sql nvarchar(4000)
-
+ 
 --Generate column names in the passed EXCEL file
-set @sql='drop table exactus.dbo.vista_presup_temp'
+set @sql='delete from exactus.dbo.vista_presup_temp'
 --exec(@sql)
-EXEC Sp_executesql @sql; 
-
-set @sql='select * into exactus.dbo.vista_presup_temp from exactus.procomer.detalle_presupuesto where ano_fecha >='+@year+' and mes_fecha <='+@month
+EXEC Sp_executesql @sql;
+ 
+set @sql='insert into exactus.dbo.vista_presup_temp  select * from exactus.procomer.detalle_presupuesto where ano_fecha >='+@year+' and mes_fecha <='+@month
 --exec(@sql)
-EXEC Sp_executesql @sql; 
-
+EXEC Sp_executesql @sql;
+ 
 set @sql='select count(*) from exactus.dbo.vista_presup_temp'
-EXEC Sp_executesql @sql; 
-GO
-
-
+EXEC Sp_executesql @sql;
